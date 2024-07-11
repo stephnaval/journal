bc6d31c1bf9c93621ffa8cdd9e63649abcda3aa6
Steph <steph@empath.ph>
2024-07-11 19:37:19 +0800

    user story 2 implement

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,35 @@
 class CategoriesController < ApplicationController
-    def create
-      category = Category.new(category_params)
-      if category.save
-        render json: category, status: :created
-      else
-        render json: category.errors, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def category_params
-      params.require(:category).permit(:name)
+  def create
+    category = Category.new(category_params)
+    if category.save
+      render json: category, status: :created
+    else
+      render json: category.errors, status: :unprocessable_entity
     end
   end
+
+  def update
+    category = Category.find(params[:id])
+    if category.update(category_params)
+      render json: category, status: :ok
+    else
+      render json: category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    category = Category.find(params[:id])
+    render json: category, status: :ok
+  end
+
+  def edit
+    category = Category.find(params[:id])
+    render json: category, status: :ok
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @category = categories(:one)  # Assuming you have a fixture file for categories
+    @category = categories(:one)
   end
 
   test "should update category" do
@@ -15,5 +15,22 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should not update category with invalid data" do
     patch category_url(@category), params: { category: { name: '' } }
     assert_response :unprocessable_entity
+  end
+
+  test "should create category" do
+    assert_difference('Category.count') do
+      post categories_url, params: { category: { name: 'Work' } }
+    end
+    assert_response :created
+  end
+
+  test "should show category" do
+    get category_url(@category)
+    assert_response :success
+  end
+
+  test "should edit category" do
+    get edit_category_url(@category)
+    assert_response :success
   end
 end
