151bfba94d450aa8fb6d480254e6406dfa83284d
Zeraphim <diamantejc87@gmail.com>
2024-08-07 16:33:52 +0800

    schema update and create db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2024_07_16_130950) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
